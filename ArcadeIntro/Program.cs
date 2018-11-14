df3cd1dfc2398cb10e6e071a87047c71ea53be53
DESKTOP-J0H0BVA\Student <travisleebarnhart@gmail.com>
2018-11-14 10:09:47 -0600

    initial commit with Add.cs included

---
+++ ArcadeIntro/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArcadeIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first integer: ");
+            int numb1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter the second integer: ");
+            int numb2 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{numb1} + {numb2} = {numb1 + numb2}");
+        }
+    }
+}
